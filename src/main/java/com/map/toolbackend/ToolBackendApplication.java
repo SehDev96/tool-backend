e8c4165c99c2fc299898410b59822d6c59197e2f
Sehran <seh96.dev@gmail.com>
2023-05-16 18:14:38 +0800

    Initial commit

---
+++ src/main/java/com/map/toolbackend/ToolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.map.toolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolBackendApplication.class, args);
+	}
+
+}
