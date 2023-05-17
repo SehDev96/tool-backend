566900d3c9b52a9541c3433cdfe6ee597a7f46e2
Sehran <seh96.dev@gmail.com>
2023-05-17 20:43:06 +0800

    changed interceptor to log instead of print

--- src/main/java/com/map/toolbackend/interceptors/RequestInterceptor.java
+++ src/main/java/com/map/toolbackend/interceptors/RequestInterceptor.java
@@ -2,26 +2,29 @@ package com.map.toolbackend.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.Date;
 
 public class RequestInterceptor implements HandlerInterceptor {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if(request.getRequestURI().equalsIgnoreCase("/app/postcode/distance")){
-            System.out.println("===========================REQUEST BEGIN===========================");
-            System.out.println("URI      : " + request.getRequestURI());
-            System.out.println("Postcode1: " + request.getParameter("postcode1"));
-            System.out.println("Postcode2: " + request.getParameter("postcode2"));
-            System.out.println("Timestamp: "+ new Timestamp(System.currentTimeMillis()));
-            System.out.println("==========================REQUEST END==============================");
+            log.info("===========================REQUEST BEGIN===========================");
+            log.info("URI      : " + request.getRequestURI());
+            log.info("Postcode1: " + request.getParameter("postcode1"));
+            log.info("Postcode2: " + request.getParameter("postcode2"));
+            log.info("Timestamp: "+ new Timestamp(System.currentTimeMillis()));
+            log.info("==========================REQUEST END==============================");
         }
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
